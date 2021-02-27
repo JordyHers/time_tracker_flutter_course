b08e5e4f9f55a4f2fddfd354297ffc19f2ead062
JordyHers <jordy925@gmail.com>
2021-02-27 22:21:11 +0300

    3.commit

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatelessWidget {
@@ -9,28 +10,34 @@ class SignInPage extends StatelessWidget {
         elevation: 0.0,
         centerTitle: true,
       ),
-      body: buildContent(),
+      body: _buildContent(),
     );
   }
 
-  Container buildContent() {
-    return Container(
-      color: Colors.yellow,
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
-            color: Colors.orange,
-            child: SizedBox(height: 100),
+          Text(
+            'Sign in',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 32.0,fontWeight: FontWeight.w500),
           ),
+
+          SizedBox(height: 8.0),
           Container(
             color: Colors.red,
             child: SizedBox(height: 100),
           ),
+          SizedBox(height: 8.0),
           Container(
             color: Colors.purple,
             child: SizedBox(height: 100),
           ),
+          SizedBox(height: 8.0),
         ],
       ),
     );

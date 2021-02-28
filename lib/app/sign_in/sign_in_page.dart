ed81551a1ae0427c84116ca80c0b5e9e121914d5
JordyHers <jordy925@gmail.com>
2021-02-28 12:11:55 +0300

    133.Preview of the Sign in and Sign out.

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
 
 class SignInPage extends StatelessWidget {
+  Future<void> _signInAnonymously() async {
+    try {
+      final userCredential = await FirebaseAuth.instance.signInAnonymously();
+      print('${userCredential.user.uid}');
+    } catch (e) {
+      print (e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +41,7 @@ class SignInPage extends StatelessWidget {
           ),
           SizedBox(height: 48.0),
           SocialSignInButton(
-            assetName:'images/google-logo.png',
+            assetName: 'images/google-logo.png',
             text: 'Sign in With Google',
             textColor: Colors.black87,
             color: Colors.white,
@@ -66,7 +76,7 @@ class SignInPage extends StatelessWidget {
             text: 'Go anonymous',
             textColor: Colors.black,
             color: Colors.lime[300],
-            onPressed: () {},
+            onPressed: _signInAnonymously,
           ),
         ],
       ),
